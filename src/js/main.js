959f088b354c139c2b23c96751e7e925af8cc748
Vladimir <vladimiraseev89@gmail.com>
2025-07-07 12:18:08 +0700

    create footer section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import "../sass/style.scss";
+
+function initInnerAccordion (){
+    const toggles = document.querySelectorAll(".footer__title");
+
+    toggles.forEach((toggle) => {
+      toggle.addEventListener("click", () => {
+        const column = toggle.closest(".footer__column");
+        column.classList.toggle("footer__column_active");
+      });
+    });
+}
+
+initInnerAccordion()
